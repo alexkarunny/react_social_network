6f955e9262168ac6b582fb52939b353adb1fbc7e
alexkarunny <smetan4ik1@gmail.com>
2023-09-11 18:15:34 +0300

    71_setState, local state

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
 import {MyPostsContainer} from './MyPosts/MyPostsContainer';
 import {ProfileType} from 'redux/profile-page-reducer';
+import ProfileStatus from 'components/Profile/ProfileInfo/profile-status';
 
 type ProfilePropsType = {
     profile?: ProfileType
@@ -12,6 +13,7 @@ export const Profile = (props: ProfilePropsType) => {
     return (
         <div>
             <h2>Profile: {props.profile?.fullName}</h2>
+            <ProfileStatus status={'I good'} />
             <ProfileInfo profile={props.profile}/>
             <MyPostsContainer
                 title={'My profile'}
